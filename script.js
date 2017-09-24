d4b7ede595e310abd2b9ee47e407d02f5942c7ec
Jacob Greenwald <jeg4t@virginia.edu>
2017-09-24 15:36:15 -0400

    cycle through three proj

--- script.js
+++ script.js
@@ -1,22 +1,33 @@
-console.log('hello world')
 var counter = 2
+console.log(counter)
+
 var first = document.getElementById('projectsContainerOne')
 var second = document.getElementById('projectsContainerTwo')
+var third = document.getElementById('projectsContainerThree')
+
 second.style.display = 'none'
+third.style.display = 'none'
 
 function nextProject () {
   switch (counter) {
     case 1:
       break
     case 2:
-      counter++
       first.style.display = 'none'
       second.style.display = 'block'
+      counter++
+      break
+    case 3:
+      counter++
+      first.style.display = 'none'
+      second.style.display = 'none'
+      third.style.display = 'block'
       break
     default:
-
   }
+  console.log(counter)
 }
+
 function prevProject () {
   switch (counter) {
     case 2:
@@ -26,8 +37,13 @@ function prevProject () {
       second.style.display = 'none'
       counter--
       break
+    case 4:
+      first.style.display = 'none'
+      second.style.display = 'block'
+      third.style.display = 'none'
+      counter--
+      break
     default:
-
   }
   console.log(counter)
 }
