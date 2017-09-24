5f56adea0ab53f6f936437514519d490209adc25
Jacob Greenwald <jeg4t@virginia.edu>
2017-09-24 15:00:35 -0400

    start toggle function

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 console.log('hello world');
+
+function nextProject () {
+  console.log('next');
+  let counter = 1
+  let first = document.getElementById('projectsContainerOne')
+  let second = document.getElementById('projectsContainerTwo')
+  console.log(counter, first, second);
+}
